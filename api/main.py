cf32e13fd7b36441a3e0f8e04ed9d606d91ec29c
Luis Junco <38411763+luisjunco@users.noreply.github.com>
2026-07-20 14:28:42 +0200

    feat: config CORS

--- api/main.py
+++ api/main.py
@@ -3,7 +3,20 @@ from fastapi import FastAPI
 from api.predictor import predict_survival
 from api.schemas import PassengerFeatures, PredictionResponse
 
+from fastapi.middleware.cors import CORSMiddleware
+
+
 app = FastAPI(title="Titanic Survival Prediction API")
+
+
+# CORS config (we'll allow requests from any origin)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=False,  # must be False if allow_origins=["*"]
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/health")
